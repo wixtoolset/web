6d1c8e489cfff769242064b10f889cd03f8c74a0
Rob Mensching <rob@robmensching.com>
2014-02-23 22:27:23 -0800

    Rename downloads site static for more uses.

--- api/Models/Configuration.cs
+++ api/Models/Configuration.cs
@@ -2,7 +2,7 @@
 {
     public class Configuration
     {
-        public const string DownloadServerFormat = "http://download.wixtoolset.org/releases/{0}/{1}";
+        public const string DownloadServerFormat = "http://static.wixtoolset.org/releases/{0}/{1}";
 
         public const string ReleasesDataFileFormat = "~/App_Data/releases/info/{0}.json";
 
