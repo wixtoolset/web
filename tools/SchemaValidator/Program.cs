9258c3dcfc486f109371ed3c3eba8f5796e6820c
Rob Mensching <rob@firegiant.com>
2023-01-02 16:22:46 -0800

    Remove unused namespaces

--- tools/SchemaValidator/Program.cs
+++ tools/SchemaValidator/Program.cs
@@ -3,12 +3,7 @@
 namespace WixBuildTools.SchemaValidator;
 
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Xml;
-using System.Xml.Linq;
 using System.Xml.Schema;
 
 public sealed class Program
